20efae45391f81a7d5b19864eafba5f1932b0330
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-07-11 10:03:50 +0200

    Passer l'application en version 0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.chasmet.lipsync"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.7.0"
+        versionCode = 10
+        versionName = "0.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
